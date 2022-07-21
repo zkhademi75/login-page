a1da2ccee4e1ed5af30190edeec9d82f651e9c86
zohre khademi <zohrekhademi9312@gmail.com>
2022-07-21 11:15:37 +0430

    login page with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Routes , Navigate} from 'react-router-dom';
+import SingUp from './component/SignUp';
+import Login from './component/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+               <Routes>
+               <Route path="/signup" element={<SingUp/>} /> 
+               <Route path="/login" element={<Login/>} /> 
+               <Route path="/" element={<Navigate to="/signup"/>} /> 
+               </Routes>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/component/Login.js
@@ -0,0 +1,78 @@
+import React , {useState , useEffect} from 'react';
+import { validate } from './validate';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {notify} from './toast';
+import styles from './SignUp.module.css'
+import { Link } from 'react-router-dom';
+
+
+
+const Login = () => {
+
+    const [data , setData] = useState ({
+        email : "" ,
+        password : "" ,
+    })
+
+    const [errors , setErrors] = useState({});
+    const [touched , setTouched] = useState({});
+
+    
+
+    useEffect(() => {
+        setErrors(validate(data , "login"))
+    } , [data , touched])
+
+    const changeHandler = event => {
+        setData({...data , [event.target.name] : event.target.value })
+    }
+
+    const focusHandler = event => {
+        setTouched({...touched , [event.target.name] : true})
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+        if(!Object.keys(errors).length){
+            notify("You loged in successfuly" , "success")
+        }else  {
+            notify("invalid data !" , "error")
+            setTouched({
+                email : true,
+                password : true,
+            })
+
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <form onSubmit={submitHandler} className={styles.formContainer}>
+                <h2 className={styles.header}>Login</h2>
+               
+                <div className={styles.formField}>
+                    <label>email :</label>
+                    <input className={(errors.email && touched.email) ? styles.uncompleted : styles.formInput}
+                     type="text" name="email" value={data.email} onChange = {changeHandler} onFocus = {focusHandler}/>
+                    {errors.email && touched.email &&<span>{errors.email}</span>}
+                </div>
+                <div className={styles.formField}>
+                    <label>password :</label>
+                    <input className={(errors.password && touched.password) ? styles.uncompleted : styles.formInput}
+                     type="password" name="password" value={data.password} onChange = {changeHandler} onFocus = {focusHandler}/>
+                    {errors.password && touched.password &&<span>{errors.password}</span>}
+                </div>
+               
+               
+                <div className={styles.formButton}>
+                    <Link to='/signup'>SignUp</Link>
+                    <button type='submit'>Login</button>
+                </div>
+            </form>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Login;
